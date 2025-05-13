78efd095a3694972b736ddca48b0607f18905442
Eddie Lee <u24843882@tuks.co.za>
2025-05-13 18:45:20 +0200

    Add search page

---
+++ pages/search.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container mx-auto p-4">
+      <h1 class="text-3xl font-bold">
+        Search Posts
+      </h1>
+      <input class="border p-2 mb-4 rounded w-full" placeholder="Search by title or author" />
+    </div>
+  </template>
